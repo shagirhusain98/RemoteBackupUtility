b47650f9e3200200cf9ad6ba7934546c9aab024c
prinshu99 <114974121+prinshu99@users.noreply.github.com>
2022-10-16 17:10:22 +0530

    Delete Airline Reservation directory

--- Airline Reservation/CUT/ToolsReport/CUNIT/include/fileA.h
+++
@@ -1,4 +0,0 @@
-#ifndef FILEA_H_
-#define FILEA_H_
-int read_customer(char arr[]); 
-#endif  /* FILEA_H_ */
--- Airline Reservation/CUT/ToolsReport/CUNIT/include/fileB.h
+++
@@ -1,4 +0,0 @@
-#ifndef FILEB_H_
-#define FILEB_H_
-int flight_data(char arr[]);
-#endif  /* FILEB_H_ */
