ad7a936b940e11c7cce767b38f4194fb87b41f05
Sriramrepaka <45929127+Sriramrepaka@users.noreply.github.com>
2022-10-19 12:13:32 +0530

    Update header.h

--- RemoteBackupUtility/CUT/CODE/include/header.h
+++ RemoteBackupUtility/CUT/CODE/include/header.h
@@ -1,64 +1,13 @@
-void send_file(char* filename, int sockfd){
-  int n;
-  char data[SIZE] = {0};
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
-  FILE* fp = fopen(filename, "r");
-  if (fp == NULL) {
-    perror("[-]Error in reading file.");
-    exit(1);
-  }
+void full();
+void full_dir();
+void incremental();
+void incremental_dir();
+void versioned();
+void versioned_dir();
+void scheduled();
+void scheduled_dir();
 
-  while(fgets(data, SIZE, fp) != NULL) {
-    if (send(sockfd, data, sizeof(data), 0) == -1) {
-      perror("[-]Error in sending file.");
-      exit(1);
-    }
-    bzero(data, SIZE);
-  }
-  strcpy(data,"0x007Xh");
-  send(sockfd, data, sizeof(data), 0);
-  bzero(data,SIZE);
-}
-
-void write_file(char* filename,int sockfd){
-  int n;
-  FILE *fp;
-  //char *filename;
-  char buffer[SIZE];
-  
- 
-  fp = fopen(filename, "w");
-  if(fp == NULL){
-  	printf("Couldnt not make file\n");
-	exit(1);
-  }
-  while (1) {
-    n = recv(sockfd, buffer, SIZE, 0);
-    if (n <= 0 || strcmp(buffer,"0x007Xh") == 0){
-      break;
-      return;
-    }
-    fprintf(fp, "%s", buffer);
-    bzero(buffer, SIZE);
-  }
-  return;
-}
-
-void spl(char* a,char* b){
-	
-	if(strstr(a+3,".") != NULL){
-		char str2[200] = "\0";
-		int n = strlen(a) - strlen(strstr(a+3,"."));
-		strncat(str2, a, n);
-		strcat(str2,"_");
-		strcat(str2,b);
-		strcat(str2,strstr(a+3,"."));
-		//printf("%s\n",str2);
-		strcpy(a,str2);
-	}
-	else{
-		strcat(a,"_");
-		strcat(a,b);
-	}
-	
-}
+#endif
